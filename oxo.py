41e8393f526777d83856d5c9c0d46d49ad80d5b0
jenkinchris11 <jenkinchris11@gmail.com>
2018-03-22 13:17:46 +0000

    Update oxo.py

--- oxo.py
+++ oxo.py
@@ -1,22 +1,49 @@
 class OxoBoard:
-    def __init__(self):
+    
+    
+    
+   
+class OxoBoard
+    grid = None
+    num_rows = 3 #Three rows
+    num_cols = 3  # Three Collumns
+    win_row = 3 # number of X an O in a row to win
+  
+
+    def __init__(self, num_rows=3, num_cols=3, win_row=3):
         """ The initialiser. Initialise any fields you need here. """
+        
+        #initialise Variables
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.win_row = win_row
+        
+        #grid
+        self.grid = [[0 for y in xrange(self.num_rows)] for x in xrange(self.num_cols)]
+        
         raise NotImplementedError("TODO: implement __init__")
 
     def get_square(self, x, y):
         """ Return 0, 1 or 2 depending on the contents of the specified square. """
-        raise NotImplementedError("TODO: implement get_square")
+        return.self.grid[X][Y]
+        
 
     def set_square(self, x, y, mark):
         """ If the specified square is currently empty (0), fill it with mark and return True.
             If the square is not empty, leave it as-is and return False. """
-        raise NotImplementedError("TODO: implement set_square")
+        if self.grid[X][Y] == self.empty;
+            self.grid[X][Y] = mark
+        
 
     def is_board_full(self):
         """ If there are still empty squares on the board, return False.
             If there are no empty squares, return True. """
-        raise NotImplementedError("TODO: implement is_board_full")
-
+        for x in xrange(self.grid_rows):
+            for y in xrange(self.grid_cols):
+                
+                if self.grid[X][Y] == self.empty:
+                    return False
+        return true
     def get_winner(self):
         """ If a player has three in a row, return 1 or 2 depending on which player.
             Otherwise, return 0. """
